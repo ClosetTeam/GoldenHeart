0d662651e6c1b246610ba9366ff320ecd9f9bcda
Neonexer <Neonexer12@gmail.com>
2024-12-19 19:20:45 +0700

    Защита на пути, возможно лишняя где юзеры

--- backend/src/cats/cats.controller.ts
+++ backend/src/cats/cats.controller.ts
@@ -1,7 +1,8 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe} from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe, UseGuards} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import {JwtAuthGuard} from "../jwt-auth.guard";
 
 @Controller('cats')
 export class CatsController {
@@ -9,6 +10,7 @@ export class CatsController {
 
   @Post()
   @UsePipes(new ValidationPipe())
+  @UseGuards(JwtAuthGuard)
   create(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
@@ -23,11 +25,13 @@ export class CatsController {
     return this.catsService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
     return this.catsService.update(+id, updateCatDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.catsService.remove(+id);

f9133b6be60b2689b935a4ab9f8351fa3f5737d3
Максим Храбрых <65070875+Neonexer@users.noreply.github.com>
2025-06-15 16:57:48 +0700

    Revert "Refactor primary button"

--- frontend/src/shared/ui/Button/Button.stories.ts
+++ frontend/src/shared/ui/Button/Button.stories.ts
@@ -1,9 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import Button from "./Button";
 
-// Определяем литеральный тип для вариантов кнопки
-type ButtonVariant = "primary" | "secondary" | "gray" | "yellow";
-
 const meta: Meta<typeof Button> = {
 	title: "Shared/Button",
 	component: Button,
@@ -13,11 +10,7 @@ const meta: Meta<typeof Button> = {
 		onClick: { action: "clicked" },
 		width: { control: "text" },
 		height: { control: "text" },
-		variant: {
-			control: "select",
-			options: ["primary", "secondary", "gray"] as ButtonVariant[],
-			description: "Вариант стиля кнопки",
-		},
+		primary: { control: "boolean" },
 	},
 };
 export default meta;
@@ -27,7 +20,7 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
 	args: {
 		label: "Primary Button",
-		variant: "primary",
+		primary: true,
 		width: "120px",
 		height: "40px",
 	},
@@ -36,25 +29,7 @@ export const Primary: Story = {
 export const Secondary: Story = {
 	args: {
 		label: "Secondary Button",
-		variant: "secondary",
-		width: "120px",
-		height: "40px",
-	},
-};
-
-export const Gray: Story = {
-	args: {
-		label: "Единоразово",
-		variant: "gray",
-		width: "120px",
-		height: "40px",
-	},
-};
-
-export const Yellow: Story = {
-	args: {
-		label: "Ежемесячно",
-		variant: "yellow",
+		primary: false,
 		width: "120px",
 		height: "40px",
 	},
